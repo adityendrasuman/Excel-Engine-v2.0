8a052f2472de700c64961b5a7d81cff26a251cdd
adityendrasuman <34791829+adityendrasuman@users.noreply.github.com>
2023-11-16 23:01:24 +0800

    Update create_new_columns_in_r.R

--- create_new_columns_in_r.R
+++ create_new_columns_in_r.R
@@ -46,9 +46,9 @@ create_new_col <- function(df_in){
   # STEP 1: Define the column
   # STEP 2: Check if the column is correctly created
   # STEP 3: Apply the skip logic on new column based on combination of existing columns using (, &, |
-    # example a: add_to_skip("new_col", "T") when no skip logic needs to be assigned
-    # example b: add_to_skip("new_col", "col1", "col2") intersection of two columns is required
-    # example c: add_to_skip("new_col", "col1 & (col2 | col3)") when a more complex combination is required
+    # example 1: add_to_skip("new_col", "T") when no skip logic needs to be assigned
+    # example 2: add_to_skip("new_col", "col1", "col2") intersection of two columns is required
+    # example 3: add_to_skip("new_col", "col1 & (col2 | col3)") when a more complex combination is required
   
   #000b: (illustration - don't delete) Unit Weight ----
   
@@ -74,6 +74,3 @@ create_new_col <- function(df_in){
   # ++++++++++++++++++++++++++ END OF SPACE FOR DEFINING NEW COLUMNS ++++++++++++++++++++++++++
   return (list(df_out, df_skip_info))
 }
-
-
-
