8ce724448148c931d71ca89d4c455893d59c6f37
Adityendra Suman <34791829+adityendrasuman@users.noreply.github.com>
2026-01-14 12:39:34 +0530

    Update 73_analysis_xy_custom.R

--- v2.1/73_analysis_xy_custom.R
+++ v2.1/73_analysis_xy_custom.R
@@ -4,7 +4,7 @@ if (!is.null(dev.list())) dev.off()
 options(survey.lonely.psu="adjust")
 cat("\014")
 start_time <- Sys.time()
-# dev.config(nstr)
+dev.config(nstr)
 
 # capture variable coming from vba ----
 args <- commandArgs(trailingOnly=T)
@@ -485,5 +485,6 @@ print(glue::glue("\n\n All done!"))
 #   print(glue::glue("Finishing in: {4 - i} sec"))
 #   Sys.sleep(1)
 # }
+
 
 
